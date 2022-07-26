4aea255e65939f83940961b7e32bed75fa3e6977
hvlopez <hectorvmlopez@gmail.com>
2022-07-25 19:23:19 -0500

    adding login screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:twich_ui_clone/widgets/custom_button.dart';
+import 'package:twich_ui_clone/widgets/custom_text_field.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
@@ -9,8 +11,54 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _passWordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final size = MediaQuery.of(context).size;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 18),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: size.height * 0.1),
+              Text(
+                "Email",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CustomTextField(controller: _emailController),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Password",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8),
+                child: CustomTextField(controller: _passWordController),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              CustomButton(onPressed: () {}, text: "Login In"),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
