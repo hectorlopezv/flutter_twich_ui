7160827c2e75a4b1e75d7d5d05de8ef112361892
hvlopez <hectorvmlopez@gmail.com>
2022-07-25 19:58:03 -0500

    login working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:twich_ui_clone/providers/user_provider.dart';
 import 'package:twich_ui_clone/screens/home_screen.dart';
 import 'package:twich_ui_clone/screens/login_screen.dart';
 import 'package:twich_ui_clone/screens/onboarding_screen.dart';
@@ -9,7 +11,16 @@ import 'package:twich_ui_clone/utils/colors.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  runApp(const MyApp());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => UserProvider(),
+        ),
+      ],
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
